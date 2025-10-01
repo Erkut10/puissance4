6664dc3cad7495509abc65dc6def889e36e6343e
Erkut10 <erkut.elyildirim@efrei.net>
2025-10-01 11:02:00 +0200

    Create puissance4.ts
    push 1

---
+++ puissance4.ts
@@ -0,0 +1,9 @@
+// Projet Puissance 4 qui se joue automatiquement et nous affiche le résultat final
+
+// on déclare nos constantes
+const LIGNES = 6;
+const COLONNES = 7;
+const VIDE = '.';
+const JOUEUR1 = 'R';
+const JOUEUR2 = 'J';
+
